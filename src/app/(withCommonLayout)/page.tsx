d75e781ea7875c00386029f915f75504ed6469a2
Rony <rony@Ronys-Air.lan>
2024-05-04 01:34:49 +0600

    specialties added

--- src/app/(withCommonLayout)/page.tsx
+++ src/app/(withCommonLayout)/page.tsx
@@ -1,10 +1,12 @@
 import HeroSection from '@/components/UI/HomePage/HeroSection/HeroSection';
+import Specialist from '@/components/UI/HomePage/Specialist/Specialist';
 import React from 'react';
 
 const HomePage = () => {
   return (
     <div>
       <HeroSection />
+      <Specialist />
     </div>
   );
 };
